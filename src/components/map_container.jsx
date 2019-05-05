f69772363255f5ee154cb9f0bd7393c08ef302d3
enz <enzo_naguiat@yahoo.com>
2019-05-05 11:38:19 +0900

    implement google-map-react to the map component.

--- src/components/map_container.jsx
+++ src/components/map_container.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const Map = (props) => {
-  return(
-    <div className="map-container">Map
-      <p>Coordinates:{props.selectedFlat.lat} {props.selectedFlat.lng}</p>
-    </div>
-  )
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+     // Important! Always set the container height explicitly
+     <div style={{ height: '100vh', width: '100%' }}>
+       <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBmlHNLD3-qWdW2_SIMOnvz3GFg7QmMZ_A' }}
+        defaultCenter={{
+          lat: this.props.selectedFlat.lat,
+          lng: this.props.selectedFlat.lng
+        }}
+        defaultZoom={11}
+       >
+         <AnyReactComponent
+           lat={59.955413}
+           lng={30.337844}
+           text="My Marker"
+         />
+       </GoogleMapReact>
+     </div>
+    );
+  }
 }
+
 
 export default Map;
