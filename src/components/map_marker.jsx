83f40f0faa5c7c47da06d97dd8a966931305d140
enz <enzo_naguiat@yahoo.com>
2019-05-06 22:04:11 +0900

    update marker styling

--- src/components/map_marker.jsx
+++ src/components/map_marker.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MapMarker = () => {
   return(
     <div>
-      <i className="fas fa-map-marker-alt" styles={{ fontSize: '40' }}></i>
+      <p className="marker"></p>
     </div>
   )
 };
